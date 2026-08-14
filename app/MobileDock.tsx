e3a56f0be0843617037a7c96ab8bf892999a97db
Ilia <benzilya@yandex.ru>
2026-08-14 16:34:19 +0300

    Add mobile bottom navigation

---
+++ app/MobileDock.tsx
@@ -0,0 +1,11 @@
+export default function MobileDock() {
+  return (
+    <nav className="mobile-dock" aria-label="Мобильная навигация">
+      <a href="#top"><span>⌂</span>Главная</a>
+      <a href="#stories"><span>▤</span>Истории</a>
+      <a href="#films"><span>▶</span>Фильмы</a>
+      <a href="#world"><span>◎</span>Мир</a>
+      <a href="#shop"><span>▱</span>Магазин</a>
+    </nav>
+  );
+}

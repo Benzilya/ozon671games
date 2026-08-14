0651cda01a456708ad4090dbd977a4794ebdb3eb
Ilia <benzilya@yandex.ru>
2026-08-14 16:39:57 +0300

    Export nested routes as html files

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: true,
+  trailingSlash: false,
   assetPrefix: isGitHubActions ? "/ozon671games/" : undefined,
   images: {
     unoptimized: true,

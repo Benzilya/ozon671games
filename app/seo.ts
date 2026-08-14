4679064f9ef1dd598ec1af8aa79a2083c0acfc65
Ilia <benzilya@yandex.ru>
2026-08-14 17:41:54 +0300

    Define canonical SEO constants

---
+++ app/seo.ts
@@ -0,0 +1,15 @@
+export const siteOrigin = "https://benzilya.github.io";
+export const siteBasePath = "/ozon671games";
+export const siteUrl = `${siteOrigin}${siteBasePath}`;
+
+export const publicRoutes = [
+  "",
+  "/audiobooks.html",
+  "/films.html",
+  "/characters.html",
+  "/timeline.html",
+  "/universe.html",
+  "/shop.html",
+  "/community.html",
+  "/stories/tihiy-den.html",
+] as const;

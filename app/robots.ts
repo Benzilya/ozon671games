aaa9d9f4933da6cfc9165d31c045d74ecb1524dd
Ilia <benzilya@yandex.ru>
2026-08-14 17:42:14 +0300

    Add robots policy

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+import { siteUrl } from "./seo";
+
+export const dynamic = "force-static";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      { userAgent: "*", allow: "/ozon671games/", disallow: ["/ozon671games/admin.html"] },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+  };
+}
